3e722062e796b6dbce9b8bf7ec44aee03935b569
Luke W Johnston <lwjohnst@gmail.com>
2016-09-14 11:51:34 -0400

    Added util fun for rounding and min_max fun

--- R/aide.R
+++ R/aide.R
@@ -1,31 +1,29 @@
-#' Aides, small and simple functions to help out.
+#' aide, small and simple functions to help out.
 #'
 #' Simple utility functions that help with data analysis.
 #'
-#' @name aides
+#' @name aide
 #' @param x Vector of values.
 #' @param y Vector of values.
 #' @param digits Number of decimal places for the output.
 #'
 NULL
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 med <- function(x, digits = 1) {
-    format(round(stats::median(x, na.rm = TRUE), digits), nsmall = digits)
+    format_round(stats::median(x, na.rm = TRUE), digits)
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 iqr <- function(x, digits = 1) {
-    paste0(
-        format(round(stats::quantile(x, 0.25, na.rm = TRUE), digits), nsmall = digits),
-        ' to ',
-        format(round(stats::quantile(x, 0.75, na.rm = TRUE), digits), nsmall = digits)
-        )
+    lower <- format_round(stats::quantile(x, 0.25, na.rm = TRUE), digits)
+    upper <- format_round(stats::quantile(x, 0.75, na.rm = TRUE), digits)
+    paste0(lower, ' to ', upper)
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @inheritParams stats::cor
 #' @export
 correlate <- function(x,
@@ -33,44 +31,55 @@ correlate <- function(x,
                       digits = 2,
                       method = 'pearson',
                       use = 'complete.obs') {
-    format(round(stats::cor(x, y, use = use,
-        method = method), digits), nsmall = digits)
+    format_round(stats::cor(x, y, use = use, method = method), digits)
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 med_iqr <- function(x, digits = 1) {
     paste0(med(x, digits), ' (', iqr(x, digits), ')')
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 average <- function(x, digits = 1) {
-    format(round(mean(x, na.rm = TRUE), digits), nsmall = digits)
+    format_round(mean(x, na.rm = TRUE), digits)
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 stddev <- function(x, digits = 1) {
-    format(round(stats::sd(x, na.rm = TRUE), digits), nsmall = digits)
+    format_round(stats::sd(x, na.rm = TRUE), digits)
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 ave_sd <- function(x, digits = 1) {
     paste0(average(x, digits), ' (', stddev(x, digits), ')')
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 tertile <- function(x) {
     cut(x, stats::quantile(x, c(0, .333, .666, 1), na.rm = TRUE),
         include.lowest = TRUE)
 }
 
-#' @rdname aides
+#' @rdname aide
 #' @export
 trim_ws <- function (x) {
     gsub("^\\s+|\\s+$", "", x)
 }
 
+#' @rdname aide
+#' @export
+min_max <- function(x, digits = 1) {
+    minimum <- format_round(min(x, na.rm = TRUE), digits)
+    maximum <- format_round(max(x, na.rm = TRUE), digits)
+    paste0(minimum, " to ", maximum)
+
+}
+
+format_round <- function(x, digits = 1) {
+    format(round(x, digits = digits), nsmall = digits)
+}
